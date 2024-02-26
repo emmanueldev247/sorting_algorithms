9a9d513f12267f357d86915a35c8a92fd048adb6
emmanueldev247 <mailemmydee@gmail.com>
2024-02-26 09:54:18 -0800

    updated: include guard used

--- sort.h
+++ sort.h
@@ -1,11 +1,8 @@
+#ifndef sort_h
+#define sort_h
+
 #include <stdlib.h>
 #include <stdio.h>
-
-
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
-void bubble_sort(int *array, size_t size);
-
 
 /**
  * struct listint_s - Doubly linked list node
@@ -20,3 +17,10 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+
+#endif
